f54e5067c6ac53b86d8cccf39cd3f5673871ddda
M4TTH3WW <matthewpascua22@gmail.com>
2024-10-17 15:00:48 +0800

    Add project files.

---
+++ Models/PhoneUser.cs
@@ -0,0 +1,11 @@
+﻿namespace Phonesmvc.Models
+{
+    public class PhoneUser
+    {
+        public string Brand { get; set; }
+
+        public string Model { get; set; }
+
+        public string Price { get; set; }
+    }
+}
